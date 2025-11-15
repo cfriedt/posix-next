zephyrproject-rtos/zephyr

commit 936d0278bdfa773390469aa102a3de004f355514

posix: fnmatch: fix bugs in fnmatch(), see issue #55186
all tests in tests/posix/c_lib_ext/src/fnmatch.c seem to pass
except one (which is still commented)

Signed-off-by: Nimish Tambe <nimisht@gmail.com>

tests/posix/c_lib_ext/src/fnmatch.c
@@ -30,14 +30,14 @@ ZTEST(posix_c_lib_ext, test_fnmatch)
 	zassert_equal(fnmatch("a*.c", "a/x.c", FNM_PATHNAME), FNM_NOMATCH);
 	zassert_ok(fnmatch("*/foo", "/foo", FNM_PATHNAME));
 	zassert_ok(fnmatch("-O[01]", "-O1", 0));
-	/* zassert_ok(fnmatch("[[?*\\]", "\\", 0)); */
-	/* zassert_ok(fnmatch("[]?*\\]", "]", 0)); */
+	zassert_ok(fnmatch("[[?*\\]", "\\", 0));
+	zassert_ok(fnmatch("[]?*\\]", "]", 0));
 	zassert_ok(fnmatch("[!]a-]", "b", 0));
 	zassert_ok(fnmatch("[]-_]", "^", 0));
 	zassert_ok(fnmatch("[!]-_]", "X", 0));
 	zassert_equal(fnmatch("??", "-", 0), FNM_NOMATCH);
 	zassert_equal(fnmatch("*LIB*", "lib", FNM_PERIOD), FNM_NOMATCH);
-	/* zassert_ok(fnmatch("a[/]b", "a/b", 0)); */
+	zassert_ok(fnmatch("a[/]b", "a/b", 0));
 	zassert_equal(fnmatch("a[/]b", "a/b", FNM_PATHNAME), FNM_NOMATCH);
 	zassert_ok(fnmatch("[a-z]/[a-z]", "a/b", 0));
 	zassert_equal(fnmatch("*", "a/b", FNM_PATHNAME), FNM_NOMATCH);
@@ -64,8 +64,8 @@ ZTEST(posix_c_lib_ext, test_fnmatch)
 	zassert_equal(fnmatch("a/?b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
 	zassert_equal(fnmatch("*a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
 	zassert_equal(fnmatch("a/*b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
-	/* zassert_equal(fnmatch("[.]a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH); */
-	/* zassert_equal(fnmatch("a/[.]b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH); */
+	zassert_equal(fnmatch("[.]a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+	zassert_equal(fnmatch("a/[.]b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
 	zassert_ok(fnmatch("*/?", "a/b", FNM_PATHNAME | FNM_PERIOD));
 	zassert_ok(fnmatch("?/*", "a/b", FNM_PATHNAME | FNM_PERIOD));
 	zassert_ok(fnmatch(".*/?", ".a/b", FNM_PATHNAME | FNM_PERIOD));

lib/posix/options/fnmatch.c
@@ -84,12 +84,15 @@ static const char *rangematch(const char *pattern, int test, int flags)
 	for (need = true, ok = false, c = FOLDCASE(*pattern++, flags); c != ']' || need;
 	     c = FOLDCASE(*pattern++, flags)) {
 		need = false;
-		if (c == '/') {
+
+		if (c == '/' && (flags & FNM_PATHNAME)) {
 			return (void *)-1;
 		}
 
 		if (c == '\\' && !(flags & FNM_NOESCAPE)) {
-			c = FOLDCASE(*pattern++, flags);
+			if (*pattern != ']' && *pattern != EOS) {
+				c = FOLDCASE(*pattern++, flags);
+			}
 		}
 
 		if (c == EOS) {
@@ -221,9 +224,20 @@ static int fnmatchx(const char *pattern, const char *string, int flags, size_t r
 				return FNM_NOMATCH;
 			}
 
-			r = rangematch(pattern, FOLDCASE(*string, flags), flags);
-			if (r == NULL) {
+			if (*string == '.' && (flags & FNM_PERIOD) &&
+			    (string == stringstart ||
+			     ((flags & FNM_PATHNAME) && *(string - 1) == '/'))) {
 				return FNM_NOMATCH;
+			}
+
+			r = rangematch(pattern, FOLDCASE(*string, flags), flags);
+
+			if (r == NULL) {
+				if (FOLDCASE('[', flags) != FOLDCASE(*string, flags)) {
+					return FNM_NOMATCH;
+				}
+				++string;
+				break;
 			}
 
 			if (r == (void *)-1) {

cfriedt/posix-next

commit 35f118ef209d2159bb197b96dad28bf464dbfb45

posix: c_lib_ext: fnmatch: fix escape-oriented regression
A regression in 936d0278bdfa773390469aa102a3de004f355514 introduced a
subtle bug in the way that escaped expressions were handled.

The regression originated with the assumption that test data (originally
adapted from a 3rd-party testsuite) was correct when it was in fact
flawed.

Specifically, `fnmatch("[[?*\\]", "\\", 0)` should fail, since the "\\"
sequence (a single backslash after compilation) escapes the following ']'
character, thus leaving the bracket expression incomplete. Since the
bracket expression (and therefore the whole pattern) is erroneous, the
call should return `FNM_NOMATCH` to indicate failure rather than 0 to
indicate success.

Added new test cases from #98827 and some commentary for subsequent
reviewers.

This change does not complete #55186 but is related to it.

Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>
Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>
Signed-off-by: Keith Packard <keithp@keithp.com>

lib/posix/c_lib_ext/fnmatch.c
@@ -102,9 +102,7 @@ static const char *rangematch(const char *pattern, int test, int flags)
 		}
 
 		if (c == '\\' && !(flags & FNM_NOESCAPE)) {
-			if (*pattern != ']' && *pattern != EOS) {
-				c = FOLDCASE(*pattern++, flags);
-			}
+			c = FOLDCASE(*pattern++, flags);
 		}
 
 		if (c == EOS) {

tests/posix/c_lib_ext/src/fnmatch.c
@@ -30,8 +30,28 @@ ZTEST(posix_c_lib_ext, test_fnmatch)
 	zassert_equal(fnmatch("a*.c", "a/x.c", FNM_PATHNAME), FNM_NOMATCH);
 	zassert_ok(fnmatch("*/foo", "/foo", FNM_PATHNAME));
 	zassert_ok(fnmatch("-O[01]", "-O1", 0));
-	zassert_ok(fnmatch("[[?*\\]", "\\", 0));
-	zassert_ok(fnmatch("[]?*\\]", "]", 0));
+	/* '\' in pattern escapes ']'. bracket expression is incomplete. error */
+	zassert_equal(fnmatch("[[?*\\]", "\\", 0), FNM_NOMATCH);
+	/* '\' in pattern does not escape ']'. bracket expression complete. '\' matches input '\' */
+	zassert_ok(fnmatch("[[?*\\]", "\\", FNM_NOESCAPE));
+	/* '\' in pattern escapes '\', match '\' */
+	zassert_ok(fnmatch("[[?*\\\\]", "\\", 0));
+	/*
+	 * "[]" (empty bracket expression) is an invalid pattern.
+	 * > The ( ']' ) shall lose its special meaning and represent itself in a bracket expression
+	 * > if it occurs first in the list (after an initial ( '^' ), if any)
+	 * https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap09.html#tag_09_03_05
+	 *
+	 * So the next test is (again) and incomplete bracket expression and should return error.
+	 * The two tests that follow it also require the ']' to be treated as a literal character to
+	 * match within the bracket expression.
+	 */
+	zassert_equal(fnmatch("[]?*\\]", "]", 0), FNM_NOMATCH);
+	/* '\' in pattern does not escape. bracket expression complete. ']' matches input ']' */
+	zassert_ok(fnmatch("[]?*\\]", "]", FNM_NOESCAPE));
+	/* '\' in pattern escapes '\'. bracket expression complete. ']' matches input ']' */
+	zassert_ok(fnmatch("[]?*\\\\]", "]", 0));
+
 	zassert_ok(fnmatch("[!]a-]", "b", 0));
 	zassert_ok(fnmatch("[]-_]", "^", 0));
 	zassert_ok(fnmatch("[!]-_]", "X", 0));