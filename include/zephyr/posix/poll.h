0108f56ebb4540850d8a6e7b371925bcaac0b15c
Chris Friedt <chris@fr4.co>
2025-11-23 13:32:08 -0500

    posix: poll: rewrite poll.h to be more conformant.
    Rewrite poll.h to be more conformant. This change includes a patch to
    Zephyr's fdtable.h to add POLLRDNORM POLLRDBAND POLLWRNORM and POLLWRBAND
    constants.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- include/zephyr/posix/poll.h
+++ include/zephyr/posix/poll.h
@@ -1,29 +1,44 @@
 /*
- * Copyright (c) 2019 Linaro Limited
+ * SPDX-FileCopyrightText: Copyright (c) The Zephyr Project Contributors
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 #ifndef ZEPHYR_INCLUDE_POSIX_POLL_H_
 #define ZEPHYR_INCLUDE_POSIX_POLL_H_
 
-#include <zephyr/net/socket.h>
+#include <zephyr/sys/fdtable.h>
+#include <zephyr/toolchain.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef	unsigned int nfds_t;
+#if defined(_POSIX_C_SOURCE) || defined(__DOXYGEN__)
 
-#define pollfd zsock_pollfd
+#if !defined(_NFDS_T_DECLARED) && !defined(__nfds_t_defined)
+typedef int nfds_t;
+#define _NFDS_T_DECLARED
+#define __nfds_t_defined
+#endif
 
-#define POLLIN ZSOCK_POLLIN
-#define POLLPRI ZSOCK_POLLPRI
-#define POLLOUT ZSOCK_POLLOUT
-#define POLLERR ZSOCK_POLLERR
-#define POLLHUP ZSOCK_POLLHUP
-#define POLLNVAL ZSOCK_POLLNVAL
+#define pollfd zvfs_pollfd
 
-int poll(struct pollfd *fds, int nfds, int timeout);
+#define POLLIN     ZVFS_POLLIN
+#define POLLRDNORM ZVFS_POLLRDNORM
+#define POLLRDBAND ZVFS_POLLRDBAND
+#define POLLPRI    ZVFS_POLLPRI
+#define POLLOUT    ZVFS_POLLOUT
+#define POLLWRNORM ZVFS_POLLWRNORM
+#define POLLWRBAND ZVFS_POLLWRBAND
+#define POLLERR    ZVFS_POLLERR
+#define POLLHUP    ZVFS_POLLHUP
+#define POLLNVAL   ZVFS_POLLNVAL
+
+BUILD_ASSERT(POLLWRNORM == POLLOUT, "POLLWRNORM must be equal to POLLOUT");
+
+int poll(struct pollfd *fds, nfds_t nfds, int timeout);
+
+#endif /* _POSIX_C_SOURCE || __DOXYGEN__ */
 
 #ifdef __cplusplus
 }
