64f85cc504bb442f1ccc2c5395ae465d806ec995
Chris Friedt <chris@fr4.co>
2025-12-05 12:39:04 -0500

    tests: posix: c_lib_ext: use zexpect instead of zassert
    When running through a number of tests, it helps to use zexpect rather
    than zassert so that we can see all failures (not just the first) after a
    CI run.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- tests/posix/c_lib_ext/src/fnmatch.c
+++ tests/posix/c_lib_ext/src/fnmatch.c
@@ -30,27 +30,27 @@
  */
 ZTEST(posix_c_lib_ext, test_fnmatch)
 {
-	zassert_ok(fnmatch("*.c", "foo.c", 0));
-	zassert_ok(fnmatch("*.c", ".c", 0));
-	zassert_equal(fnmatch("*.a", "foo.c", 0), FNM_NOMATCH);
-	zassert_ok(fnmatch("*.c", ".foo.c", 0));
-	zassert_equal(fnmatch("*.c", ".foo.c", FNM_PERIOD), FNM_NOMATCH);
-	zassert_ok(fnmatch("*.c", "foo.c", FNM_PERIOD));
-	zassert_equal(fnmatch("a\\*.c", "a*.c", FNM_NOESCAPE), FNM_NOMATCH);
-	zassert_equal(fnmatch("a\\*.c", "ax.c", 0), FNM_NOMATCH);
-	zassert_ok(fnmatch("a[xy].c", "ax.c", 0));
-	zassert_ok(fnmatch("a[!y].c", "ax.c", 0));
-	zassert_equal(fnmatch("a[a/z]*.c", "a/x.c", FNM_PATHNAME), FNM_NOMATCH);
-	zassert_ok(fnmatch("a/*.c", "a/x.c", FNM_PATHNAME));
-	zassert_equal(fnmatch("a*.c", "a/x.c", FNM_PATHNAME), FNM_NOMATCH);
-	zassert_ok(fnmatch("*/foo", "/foo", FNM_PATHNAME));
-	zassert_ok(fnmatch("-O[01]", "-O1", 0));
+	zexpect_ok(fnmatch("*.c", "foo.c", 0));
+	zexpect_ok(fnmatch("*.c", ".c", 0));
+	zexpect_equal(fnmatch("*.a", "foo.c", 0), FNM_NOMATCH);
+	zexpect_ok(fnmatch("*.c", ".foo.c", 0));
+	zexpect_equal(fnmatch("*.c", ".foo.c", FNM_PERIOD), FNM_NOMATCH);
+	zexpect_ok(fnmatch("*.c", "foo.c", FNM_PERIOD));
+	zexpect_equal(fnmatch("a\\*.c", "a*.c", FNM_NOESCAPE), FNM_NOMATCH);
+	zexpect_equal(fnmatch("a\\*.c", "ax.c", 0), FNM_NOMATCH);
+	zexpect_ok(fnmatch("a[xy].c", "ax.c", 0));
+	zexpect_ok(fnmatch("a[!y].c", "ax.c", 0));
+	zexpect_equal(fnmatch("a[a/z]*.c", "a/x.c", FNM_PATHNAME), FNM_NOMATCH);
+	zexpect_ok(fnmatch("a/*.c", "a/x.c", FNM_PATHNAME));
+	zexpect_equal(fnmatch("a*.c", "a/x.c", FNM_PATHNAME), FNM_NOMATCH);
+	zexpect_ok(fnmatch("*/foo", "/foo", FNM_PATHNAME));
+	zexpect_ok(fnmatch("-O[01]", "-O1", 0));
 	/* '\' in pattern escapes ']'. bracket expression is incomplete. error */
-	zassert_equal(fnmatch("[[?*\\]", "\\", 0), FNM_NOMATCH);
+	zexpect_equal(fnmatch("[[?*\\]", "\\", 0), FNM_NOMATCH);
 	/* '\' in pattern does not escape ']'. bracket expression complete. '\' matches input '\' */
-	zassert_ok(fnmatch("[[?*\\]", "\\", FNM_NOESCAPE));
+	zexpect_ok(fnmatch("[[?*\\]", "\\", FNM_NOESCAPE));
 	/* '\' in pattern escapes '\', match '\' */
-	zassert_ok(fnmatch("[[?*\\\\]", "\\", 0));
+	zexpect_ok(fnmatch("[[?*\\\\]", "\\", 0));
 	/*
 	 * "[]" (empty bracket expression) is an invalid pattern.
 	 * > The ( ']' ) shall lose its special meaning and represent itself in a bracket expression
@@ -61,60 +61,62 @@ ZTEST(posix_c_lib_ext, test_fnmatch)
 	 * The two tests that follow it also require the ']' to be treated as a literal character to
 	 * match within the bracket expression.
 	 */
-	zassert_equal(fnmatch("[]?*\\]", "]", 0), FNM_NOMATCH);
+	zexpect_equal(fnmatch("[]?*\\]", "]", 0), FNM_NOMATCH);
 	/* '\' in pattern does not escape. bracket expression complete. ']' matches input ']' */
-	zassert_ok(fnmatch("[]?*\\]", "]", FNM_NOESCAPE));
+	zexpect_ok(fnmatch("[]?*\\]", "]", FNM_NOESCAPE));
 	/* '\' in pattern escapes '\'. bracket expression complete. ']' matches input ']' */
-	zassert_ok(fnmatch("[]?*\\\\]", "]", 0));
+	zexpect_ok(fnmatch("[]?*\\\\]", "]", 0));
 
-	zassert_ok(fnmatch("[!]a-]", "b", 0));
-	zassert_ok(fnmatch("[]-_]", "^", 0));
-	zassert_ok(fnmatch("[!]-_]", "X", 0));
-	zassert_equal(fnmatch("??", "-", 0), FNM_NOMATCH);
-	zassert_equal(fnmatch("*LIB*", "lib", FNM_PERIOD), FNM_NOMATCH);
-	zassert_ok(fnmatch("a[/]b", "a/b", 0));
-	zassert_equal(fnmatch("a[/]b", "a/b", FNM_PATHNAME), FNM_NOMATCH);
-	zassert_ok(fnmatch("[a-z]/[a-z]", "a/b", 0));
-	zassert_equal(fnmatch("*", "a/b", FNM_PATHNAME), FNM_NOMATCH);
-	zassert_equal(fnmatch("*[/]b", "a/b", FNM_PATHNAME), FNM_NOMATCH);
-	zassert_equal(fnmatch("*[b]", "a/b", FNM_PATHNAME), FNM_NOMATCH);
-	zassert_equal(fnmatch("[*]/b", "a/b", 0), FNM_NOMATCH);
-	zassert_ok(fnmatch("[*]/b", "*/b", 0));
-	zassert_equal(fnmatch("[?]/b", "a/b", 0), FNM_NOMATCH);
-	zassert_ok(fnmatch("[?]/b", "?/b", 0));
-	zassert_ok(fnmatch("[[a]/b", "a/b", 0));
-	zassert_ok(fnmatch("[[a]/b", "[/b", 0));
-	zassert_equal(fnmatch("\\*/b", "a/b", 0), FNM_NOMATCH);
-	zassert_ok(fnmatch("\\*/b", "*/b", 0));
-	zassert_equal(fnmatch("\\?/b", "a/b", 0), FNM_NOMATCH);
-	zassert_ok(fnmatch("\\?/b", "?/b", 0));
-	zassert_ok(fnmatch("[/b", "[/b", 0));
-	zassert_ok(fnmatch("\\[/b", "[/b", 0));
-	zassert_ok(fnmatch("??"
+	zexpect_ok(fnmatch("[!]a-]", "b", 0));
+	zexpect_ok(fnmatch("[]-_]", "^", 0));
+	zexpect_ok(fnmatch("[!]-_]", "X", 0));
+	zexpect_equal(fnmatch("??", "-", 0), FNM_NOMATCH);
+	zexpect_equal(fnmatch("*LIB*", "lib", FNM_PERIOD), FNM_NOMATCH);
+	zexpect_ok(fnmatch("a[/]b", "a/b", 0));
+	zexpect_equal(fnmatch("a[/]b", "a/b", FNM_PATHNAME), FNM_NOMATCH);
+	zexpect_ok(fnmatch("[a-z]/[a-z]", "a/b", 0));
+	zexpect_equal(fnmatch("*", "a/b", FNM_PATHNAME), FNM_NOMATCH);
+	zexpect_equal(fnmatch("*[/]b", "a/b", FNM_PATHNAME), FNM_NOMATCH);
+	zexpect_equal(fnmatch("*[b]", "a/b", FNM_PATHNAME), FNM_NOMATCH);
+	zexpect_equal(fnmatch("[*]/b", "a/b", 0), FNM_NOMATCH);
+	zexpect_ok(fnmatch("[*]/b", "*/b", 0));
+	zexpect_equal(fnmatch("[?]/b", "a/b", 0), FNM_NOMATCH);
+	zexpect_ok(fnmatch("[?]/b", "?/b", 0));
+	zexpect_ok(fnmatch("[[a]/b", "a/b", 0));
+	zexpect_ok(fnmatch("[[a]/b", "[/b", 0));
+	zexpect_equal(fnmatch("\\*/b", "a/b", 0), FNM_NOMATCH);
+	zexpect_ok(fnmatch("\\*/b", "*/b", 0));
+	zexpect_equal(fnmatch("\\?/b", "a/b", 0), FNM_NOMATCH);
+	zexpect_ok(fnmatch("\\?/b", "?/b", 0));
+	zexpect_ok(fnmatch("[/b", "[/b", 0));
+	zexpect_ok(fnmatch("\\[/b", "[/b", 0));
+	zexpect_ok(fnmatch("??"
 			   "/b",
 			   "aa/b", 0));
-	zassert_ok(fnmatch("???b", "aa/b", 0));
-	zassert_equal(fnmatch("???b", "aa/b", FNM_PATHNAME), FNM_NOMATCH);
-	zassert_equal(fnmatch("?a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
-	zassert_equal(fnmatch("a/?b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
-	zassert_equal(fnmatch("*a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
-	zassert_equal(fnmatch("a/*b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
-	zassert_equal(fnmatch("[.]a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
-	zassert_equal(fnmatch("a/[.]b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
-	zassert_ok(fnmatch("*/?", "a/b", FNM_PATHNAME | FNM_PERIOD));
-	zassert_ok(fnmatch("?/*", "a/b", FNM_PATHNAME | FNM_PERIOD));
-	zassert_ok(fnmatch(".*/?", ".a/b", FNM_PATHNAME | FNM_PERIOD));
-	zassert_ok(fnmatch("*/.?", "a/.b", FNM_PATHNAME | FNM_PERIOD));
-	zassert_equal(fnmatch("*/*", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
-	zassert_ok(fnmatch("*?*/*", "a/.b", FNM_PERIOD));
-	zassert_ok(fnmatch("*[.]/b", "a./b", FNM_PATHNAME | FNM_PERIOD));
-	zassert_ok(fnmatch("*[[:alpha:]]/" "*[[:alnum:]]", "a/b", FNM_PATHNAME));
-	zassert_ok(fnmatch("*[![:digit:]]*/[![:d-d]", "a/b", FNM_PATHNAME), 0);
-	zassert_ok(fnmatch("*[![:digit:]]*/[[:d-d]", "a/[", FNM_PATHNAME), 0);
-	zassert_equal(fnmatch("*[![:digit:]]*/[![:d-d]", "a/[", FNM_PATHNAME), FNM_NOMATCH);
-	zassert_ok(fnmatch("a?b", "a.b", FNM_PATHNAME | FNM_PERIOD));
-	zassert_ok(fnmatch("a*b", "a.b", FNM_PATHNAME | FNM_PERIOD));
-	zassert_ok(fnmatch("a[.]b", "a.b", FNM_PATHNAME | FNM_PERIOD));
+	zexpect_ok(fnmatch("???b", "aa/b", 0));
+	zexpect_equal(fnmatch("???b", "aa/b", FNM_PATHNAME), FNM_NOMATCH);
+	zexpect_equal(fnmatch("?a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+	zexpect_equal(fnmatch("a/?b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+	zexpect_equal(fnmatch("*a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+	zexpect_equal(fnmatch("a/*b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+	zexpect_equal(fnmatch("[.]a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+	zexpect_equal(fnmatch("a/[.]b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+	zexpect_ok(fnmatch("*/?", "a/b", FNM_PATHNAME | FNM_PERIOD));
+	zexpect_ok(fnmatch("?/*", "a/b", FNM_PATHNAME | FNM_PERIOD));
+	zexpect_ok(fnmatch(".*/?", ".a/b", FNM_PATHNAME | FNM_PERIOD));
+	zexpect_ok(fnmatch("*/.?", "a/.b", FNM_PATHNAME | FNM_PERIOD));
+	zexpect_equal(fnmatch("*/*", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+	zexpect_ok(fnmatch("*?*/*", "a/.b", FNM_PERIOD));
+	zexpect_ok(fnmatch("*[.]/b", "a./b", FNM_PATHNAME | FNM_PERIOD));
+	zexpect_ok(fnmatch("*[[:alpha:]]/"
+			   "*[[:alnum:]]",
+			   "a/b", FNM_PATHNAME));
+	zexpect_ok(fnmatch("*[![:digit:]]*/[![:d-d]", "a/b", FNM_PATHNAME), 0);
+	zexpect_ok(fnmatch("*[![:digit:]]*/[[:d-d]", "a/[", FNM_PATHNAME), 0);
+	zexpect_equal(fnmatch("*[![:digit:]]*/[![:d-d]", "a/[", FNM_PATHNAME), FNM_NOMATCH);
+	zexpect_ok(fnmatch("a?b", "a.b", FNM_PATHNAME | FNM_PERIOD));
+	zexpect_ok(fnmatch("a*b", "a.b", FNM_PATHNAME | FNM_PERIOD));
+	zexpect_ok(fnmatch("a[.]b", "a.b", FNM_PATHNAME | FNM_PERIOD));
 
 	/* Additional test cases for POSIX character classes (C-locale only) */
 	static const struct test_data_s {
@@ -160,5 +162,5 @@ ZTEST(posix_c_lib_ext, test_fnmatch)
 	}
 
 	/* ensure that an invalid character class generates an error */
-	zassert_equal(fnmatch("[[:foobarbaz:]]", "Z", 0), FNM_NOMATCH);
+	zexpect_equal(fnmatch("[[:foobarbaz:]]", "Z", 0), FNM_NOMATCH);
 }
