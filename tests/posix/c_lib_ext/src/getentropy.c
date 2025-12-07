3a178cb9d98b4d9dd8000db19698208c3ed21734
Chris Friedt <chris@fr4.co>
2025-12-07 11:14:24 -0500

    tests: getentropy: combine tests into one
    Combine getentropy tests into one and skip portions of the testsuite
    when there is no Zephyr PRNG device.
    
    By skipping tests in the testsuite and not filtering in testcase.yaml, we
    increase test coverage for other functions within the POSIX_C_LIB_EXT
    Option Group.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- tests/posix/c_lib_ext/src/getentropy.c
+++ tests/posix/c_lib_ext/src/getentropy.c
@@ -7,48 +7,49 @@
 #include <zephyr/posix/unistd.h>
 #include <zephyr/ztest.h>
 
-ZTEST(posix_c_lib_ext, test_getentropy_too_large)
-{
-	uint8_t buf[256 + 1] = {0};
-	int ret;
-
-	ret = getentropy(buf, sizeof(buf));
-	zassert_equal(ret, -1);
-	zassert_equal(errno, EIO);
-}
-
-ZTEST(posix_c_lib_ext, test_getentropy_null_buffer)
-{
-	int ret;
-
-	ret = getentropy(NULL, 0);
-	zassert_equal(ret, -1);
-	zassert_equal(errno, EFAULT);
-}
-
-ZTEST(posix_c_lib_ext, test_getentropy_max_size)
-{
-	uint8_t buf[256] = {0};
-	int ret;
-
-	ret = getentropy(buf, sizeof(buf));
-	zassert_equal(ret, 0);
-}
+static const struct device *const dev = DEVICE_DT_GET_OR_NULL(DT_CHOSEN(zephyr_entropy));
 
 ZTEST(posix_c_lib_ext, test_getentropy)
 {
-	uint8_t zero[16] = {0};
-	uint8_t buf1[16];
-	uint8_t buf2[16];
-	int ret;
+	/* verify that a buffer larger than 256 bytes returns an error */
+	{
+		uint8_t buf[256 + 1] = {0};
+		int ret;
 
-	ret = getentropy(buf1, sizeof(buf1));
-	zassert_equal(ret, 0);
+		ret = getentropy(buf, sizeof(buf));
+		zexpect_equal(ret, -1);
+		zexpect_equal(errno, EIO);
+	}
 
-	ret = getentropy(buf2, sizeof(buf2));
-	zassert_equal(ret, 0);
+	if ((dev == NULL) || !device_is_ready(dev)) {
+		/* some platforms do not have an entropy device, so skip the tests below */
+		ztest_test_skip();
+	}
 
-	zassert_true(memcmp(buf1, zero, sizeof(zero)) != 0);
-	zassert_true(memcmp(buf2, zero, sizeof(zero)) != 0);
-	zassert_true(memcmp(buf1, buf2, sizeof(buf1)) != 0);
+	/* verify that a buffer of 256 bytes returns success */
+	{
+		uint8_t buf[256] = {0};
+		int ret;
+
+		ret = getentropy(buf, sizeof(buf));
+		zexpect_equal(ret, 0);
+	}
+
+	/* verify that two calls return different data */
+	{
+		uint8_t zero[16] = {0};
+		uint8_t buf1[16];
+		uint8_t buf2[16];
+		int ret;
+
+		ret = getentropy(buf1, sizeof(buf1));
+		zassert_equal(ret, 0);
+
+		ret = getentropy(buf2, sizeof(buf2));
+		zassert_equal(ret, 0);
+
+		zassert_true(memcmp(buf1, zero, sizeof(zero)) != 0);
+		zassert_true(memcmp(buf2, zero, sizeof(zero)) != 0);
+		zassert_true(memcmp(buf1, buf2, sizeof(buf1)) != 0);
+	}
 }
