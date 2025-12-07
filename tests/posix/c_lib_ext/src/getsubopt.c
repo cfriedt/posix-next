aa2ff3f3ada1a68a77d776e0a9140559fccd3121
Chris Friedt <chris@fr4.co>
2025-12-07 10:28:48 -0500

    posix: c_lib_ext: getsubopt: assert that args are non-NULL
    Adjust getsubopt() implementaiton to assert that arguments are non-NULL.
    
    Additionally, adjust the test to avoid degenerate inputs, as NULL input
    parameters cause some implementations to fault.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- tests/posix/c_lib_ext/src/getsubopt.c
+++ tests/posix/c_lib_ext/src/getsubopt.c
@@ -35,104 +35,62 @@ ZTEST(posix_c_lib_ext, test_getsubopt)
 		NULL,
 	};
 
-	if (IS_ENABLED(CONFIG_ARCH_POSIX)) {
-		/* zassert_equal(0, strcmp(argv[GETOPT_IDX_OPTARG], state->optarg), causes a
-		 * segfault on native_sim ¯\_(ツ)_/¯
-		 */
-		ztest_test_skip();
+	/* empty options string */
+	memset(buf, 0, sizeof(buf));
+	option = buf;
+	value = (char *)0x4242;
+	zexpect_equal(-1, getsubopt(&option, (char **)key_list, &value));
+	/* empty key list */
+	strcpy(buf, "ro,rsize=512,equal=1=2,rw");
+	option = buf;
+	value = (char *)0x4242;
+	zexpect_equal(-1, getsubopt(&option, (char **)empty_key_list, &value));
+
+	strcpy(buf, "ro,rsize=512,equal=1=2,rw");
+	option = buf;
+	value = (char *)0x4242;
+	zexpect_equal(RO_OPTION, getsubopt(&option, (char **)key_list, &value));
+	zexpect_equal(option, &buf[strlen("ro,")]);
+
+	zexpect_equal(READ_SIZE_OPTION, getsubopt(&option, (char **)key_list, &value));
+	zexpect_equal(option, &buf[strlen("ro,rsize=512,")]);
+	zexpect_not_null(value);
+	if (value != NULL) {
+		zexpect_str_equal(value, "512");
 	}
 
-	/* degenerate cases */
-#if !defined(CONFIG_NATIVE_LIBC)
-	zexpect_equal(-1, getsubopt(NULL, NULL, NULL));
-	zexpect_equal(-1, getsubopt(NULL, NULL, &value));
-	zexpect_equal(-1, getsubopt(NULL, (char **)key_list, NULL));
-	zexpect_equal(-1, getsubopt(NULL, (char **)key_list, &value));
-	zexpect_equal(-1, getsubopt(&option, NULL, NULL));
-	zexpect_equal(-1, getsubopt(&option, NULL, &value));
-#endif
+	value = (char *)0x4242;
+	zexpect_equal(EMBEDDED_EQ_OPTION, getsubopt(&option, (char **)key_list, &value));
+	zexpect_equal(option, &buf[strlen("ro,rsize=512,equal=1=2,")]);
+	zexpect_not_null(value);
+	if (value != NULL) {
+		zexpect_str_equal(value, "1=2");
+	}
 
-	/* iterate over valuep being NULL and valid */
-	for (size_t j = 0; j < 2; ++j) {
-		char **valuep = (j == 0) ? NULL : &value;
+	value = (char *)0x4242;
+	zexpect_equal(RW_OPTION, getsubopt(&option, (char **)key_list, &value));
+	zexpect_equal(option, &buf[strlen("ro,rsize=512,equal=1=2,rw")]);
 
-		/* empty options string */
-		memset(buf, 0, sizeof(buf));
-		option = buf;
-		value = (char *)0x4242;
-		zexpect_equal(-1, getsubopt(&option, (char **)key_list, valuep));
-		if (valuep != NULL) {
-			zexpect_equal(value, NULL);
-		}
+	value = (char *)0x4242;
+	zexpect_equal(getsubopt(&option, (char **)key_list, &value), -1);
 
-		/* empty key list */
-		strcpy(buf, "ro,rsize=512,equal=1=2,rw");
-		option = buf;
-		value = (char *)0x4242;
-		zexpect_equal(-1, getsubopt(&option, (char **)empty_key_list, valuep));
-		if (valuep != NULL) {
-			zexpect_equal(value, NULL);
-		}
+	strcpy(buf, "oops");
+	option = buf;
+	value = (char *)0x4242;
+	zexpect_equal(getsubopt(&option, (char **)key_list, &value), -1);
 
-		strcpy(buf, "ro,rsize=512,equal=1=2,rw");
-		option = buf;
-		value = (char *)0x4242;
-		zexpect_equal(RO_OPTION, getsubopt(&option, (char **)key_list, valuep));
-		zexpect_equal(option, &buf[strlen("ro,")]);
-		if (valuep != NULL) {
-			zexpect_equal(value, NULL);
-		}
+	/* some corner cases */
+	strcpy(buf, ",rsize=,");
+	option = buf;
+	value = (char *)0x4242;
+	zexpect_equal(-1, getsubopt(&option, (char **)key_list, &value));
+	zexpect_equal(option, &buf[strlen(",")]);
 
-		value = (char *)0x4242;
-		zexpect_equal(READ_SIZE_OPTION, getsubopt(&option, (char **)key_list, valuep));
-		zexpect_equal(option, &buf[strlen("ro,rsize=512,")]);
-		if (valuep != NULL) {
-			zexpect_str_equal(value, "512");
-		}
-
-		value = (char *)0x4242;
-		zexpect_equal(EMBEDDED_EQ_OPTION, getsubopt(&option, (char **)key_list, valuep));
-		zexpect_equal(option, &buf[strlen("ro,rsize=512,equal=1=2,")]);
-		if (valuep != NULL) {
-			zexpect_str_equal(value, "1=2");
-		}
-
-		value = (char *)0x4242;
-		zexpect_equal(RW_OPTION, getsubopt(&option, (char **)key_list, valuep));
-		zexpect_equal(option, &buf[strlen("ro,rsize=512,equal=1=2,rw")]);
-		if (valuep != NULL) {
-			zexpect_equal(value, NULL);
-		}
-
-		value = (char *)0x4242;
-		zexpect_equal(getsubopt(&option, (char **)key_list, valuep), -1);
-		if (valuep != NULL) {
-			zexpect_equal(value, NULL);
-		}
-
-		strcpy(buf, "oops");
-		option = buf;
-		value = (char *)0x4242;
-		zexpect_equal(getsubopt(&option, (char **)key_list, valuep), -1);
-		if (valuep != NULL) {
-			zexpect_equal(value, NULL);
-		}
-
-		/* some corner cases */
-		strcpy(buf, ",rsize=,");
-		option = buf;
-		value = (char *)0x4242;
-		zexpect_equal(-1, getsubopt(&option, (char **)key_list, valuep));
-		zexpect_equal(option, &buf[strlen(",")]);
-		if (valuep != NULL) {
-			zexpect_equal(value, NULL);
-		}
-
-		value = (char *)0x4242;
-		zexpect_equal(READ_SIZE_OPTION, getsubopt(&option, (char **)key_list, valuep));
-		zexpect_equal(option, &buf[strlen(",rsize=,")]);
-		if (valuep != NULL) {
-			zexpect_str_equal(value, "");
-		}
+	value = (char *)0x4242;
+	zexpect_equal(READ_SIZE_OPTION, getsubopt(&option, (char **)key_list, &value));
+	zexpect_equal(option, &buf[strlen(",rsize=,")]);
+	zexpect_not_null(value);
+	if (value != NULL) {
+		zexpect_str_equal(value, "");
 	}
 }
