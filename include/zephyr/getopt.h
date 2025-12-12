eeb569e77a9acc67514c906c85599a0bddb1b579
Chris Friedt <chris@fr4.co>
2025-12-12 16:40:43 -0500

    posix: c_lib_ext: getopt: add zephyr_getopt()
    Add `zephyr_getopt()` for support for `getopt_long()` and
    `getopt_long_only()`.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

---
+++ include/zephyr/getopt.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) The Zephyr Project Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_GETOPT_H_
+#define ZEPHYR_GETOPT_H_
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <zephyr/sys/util_macro.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/* struct option is defined in the libc's getopt.h header */
+struct option;
+
+int zephyr_getopt(int argc, char *const argv[], const char *optstring,
+		  const struct option *longopts, int *longindex, bool longonly, char **optarg,
+		  int *opterr, int *optind, int *optopt);
+
+static inline int getopt_r(int argc, char *const argv[], const char *optstring, char **optarg,
+			   int *opterr, int *optind, int *optopt)
+{
+	if (!IS_ENABLED(CONFIG_ZEPHYR_GETOPT)) {
+		return -1;
+	}
+
+	return zephyr_getopt(argc, argv, optstring, NULL, NULL, false, optarg, opterr, optind,
+			     optopt);
+}
+
+static inline int getopt_long_r(int argc, char *const argv[], const char *optstring,
+				const struct option *longopts, int *longindex, char **optarg,
+				int *opterr, int *optind, int *optopt)
+{
+	if (!IS_ENABLED(CONFIG_ZEPHYR_GETOPT)) {
+		return -1;
+	}
+
+	return zephyr_getopt(argc, argv, optstring, longopts, longindex, false, optarg, opterr,
+			     optind, optopt);
+}
+
+static inline int getopt_long_only_r(int argc, char *const argv[], const char *optstring,
+				     const struct option *longopts, int *longindex, char **optarg,
+				     int *opterr, int *optind, int *optopt)
+{
+	if (!IS_ENABLED(CONFIG_ZEPHYR_GETOPT)) {
+		return -1;
+	}
+
+	return zephyr_getopt(argc, argv, optstring, longopts, longindex, true, optarg, opterr,
+			     optind, optopt);
+}
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
