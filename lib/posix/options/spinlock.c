2e4ff10680a628d9510125a57cff5bf7ee932808
Chris Friedt <chris@fr4.co>
2025-12-18 10:05:00 -0500

    posix: pthread: elastipool for object pools
    Use the newly introduced elastipool elastic object pool for management
    of
    
    * barrier
    * cond
    * key
    * mutex
    * pthread
    * rwlock
    * spinlock
    
    objects.
    
    This change only changes the storage API used for those areas from
    an array + bitarray to an elastipool, which is effectively the same thing,
    but without spinlock synchronization. The main reason for doing so is that
    spinlocks cannot be used from userspace code, so synchronization likely
    does not work as expected in that case. Instead of the built-in spinlock
    used with the bitarray API, use an external sys_sem for locking, which
    may be used from userspace code.
    
    This does not enable heap allocation of objects, as the min / max values
    remain equal to their original Kconfig limits. Since min == max, all
    allocations are static / guaranteed as part of the statically allocated
    "slab" storage.
    
    One "drive-by" update was made - specifically, to use atomics in the
    POSIX spinlock implementation, instead of kernel spinlocks.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- lib/posix/options/spinlock.c
+++ lib/posix/options/spinlock.c
@@ -10,72 +10,17 @@
 
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
-#include <zephyr/sys/bitarray.h>
+#include <zephyr/sys/atomic.h>
+#include <zephyr/sys/elastipool.h>
+#include <zephyr/sys/sem.h>
 
-union _spinlock_storage {
-	struct k_spinlock lock;
-	uint8_t byte;
-};
-#if !defined(CONFIG_CPP) && !defined(CONFIG_SMP) && !defined(CONFIG_SPIN_VALIDATE)
-BUILD_ASSERT(sizeof(struct k_spinlock) == 0,
-	     "please remove the _spinlock_storage workaround if, at some point, k_spinlock is no "
-	     "longer zero bytes when CONFIG_SMP=n && CONFIG_SPIN_VALIDATE=n");
-#endif
-
-static union _spinlock_storage posix_spinlock_pool[CONFIG_MAX_PTHREAD_SPINLOCK_COUNT];
-static k_spinlock_key_t posix_spinlock_key[CONFIG_MAX_PTHREAD_SPINLOCK_COUNT];
-SYS_BITARRAY_DEFINE_STATIC(posix_spinlock_bitarray, CONFIG_MAX_PTHREAD_SPINLOCK_COUNT);
-
-/*
- * We reserve the MSB to mark a pthread_spinlock_t as initialized (from the
- * perspective of the application). With a linear space, this means that
- * the theoretical pthread_spinlock_t range is [0,2147483647].
- */
-BUILD_ASSERT(CONFIG_MAX_PTHREAD_SPINLOCK_COUNT < PTHREAD_OBJ_MASK_INIT,
-	"CONFIG_MAX_PTHREAD_SPINLOCK_COUNT is too high");
-
-static inline size_t posix_spinlock_to_offset(struct k_spinlock *l)
-{
-	return (union _spinlock_storage *)l - posix_spinlock_pool;
-}
-
-static inline size_t to_posix_spinlock_idx(pthread_spinlock_t lock)
-{
-	return mark_pthread_obj_uninitialized(lock);
-}
-
-static struct k_spinlock *get_posix_spinlock(pthread_spinlock_t *lock)
-{
-	size_t bit;
-	int actually_initialized;
-
-	if (lock == NULL) {
-		return NULL;
-	}
-
-	/* if the provided spinlock does not claim to be initialized, its invalid */
-	bit = to_posix_spinlock_idx(*lock);
-	if (!is_pthread_obj_initialized(*lock)) {
-		return NULL;
-	}
-
-	/* Mask off the MSB to get the actual bit index */
-	if (sys_bitarray_test_bit(&posix_spinlock_bitarray, bit, &actually_initialized) < 0) {
-		return NULL;
-	}
-
-	if (actually_initialized == 0) {
-		/* The spinlock claims to be initialized but is actually not */
-		return NULL;
-	}
-
-	return (struct k_spinlock *)&posix_spinlock_pool[bit];
-}
+static SYS_SEM_DEFINE(posix_spin_lock, 1, 1);
+SYS_ELASTIPOOL_DEFINE_STATIC(posix_spin_pool, sizeof(atomic_t), __alignof(atomic_t),
+			     CONFIG_MAX_PTHREAD_SPINLOCK_COUNT, CONFIG_MAX_PTHREAD_SPINLOCK_COUNT);
 
 int pthread_spin_init(pthread_spinlock_t *lock, int pshared)
 {
-	int ret;
-	size_t bit;
+	int ret = EINVAL;
 
 	if (lock == NULL ||
 	    !(pshared == PTHREAD_PROCESS_PRIVATE || pshared == PTHREAD_PROCESS_SHARED)) {
@@ -83,80 +28,114 @@ int pthread_spin_init(pthread_spinlock_t *lock, int pshared)
 		return EINVAL;
 	}
 
-	ret = sys_bitarray_alloc(&posix_spinlock_bitarray, 1, &bit);
-	if (ret < 0) {
-		return ENOMEM;
+	SYS_SEM_LOCK(&posix_spin_lock) {
+		uintptr_t l = 0;
+
+		ret = sys_elastipool_alloc(&posix_spin_pool, (void **)&l);
+		if (ret < 0) {
+			ret = ENOMEM;
+		} else {
+			atomic_set((atomic_t *)l, 0);
+			*lock = (pthread_spinlock_t)(uintptr_t)l;
+		}
 	}
 
-	*lock = mark_pthread_obj_initialized(bit);
-
-	return 0;
+	return ret;
 }
 
 int pthread_spin_destroy(pthread_spinlock_t *lock)
 {
-	int err;
-	size_t bit;
-	struct k_spinlock *l;
+	atomic_t *l;
+	int ret = EINVAL;
 
-	l = get_posix_spinlock(lock);
-	if (l == NULL) {
-		/* not specified as part of POSIX but this is the Linux behavior */
+	if (lock == NULL) {
 		return EINVAL;
 	}
 
-	bit = posix_spinlock_to_offset(l);
-	err = sys_bitarray_free(&posix_spinlock_bitarray, 1, bit);
-	__ASSERT_NO_MSG(err == 0);
+	SYS_SEM_LOCK(&posix_spin_lock) {
+		l = posix_get_pool_obj_unlocked(&posix_spin_pool, *lock);
+		if (l == NULL) {
+			/* not specified as part of POSIX but this is the Linux behavior */
+			ret = EINVAL;
+			SYS_SEM_LOCK_BREAK;
+		}
 
-	return 0;
+		ret = sys_elastipool_free(&posix_spin_pool, (void *)l);
+		__ASSERT_NO_MSG(ret == 0);
+	}
+
+	return ret;
+}
+
+static int pthread_spin_lock_common(pthread_spinlock_t *lock, bool wait)
+{
+	atomic_t *l;
+	int ret = EINVAL;
+
+	if (lock == NULL) {
+		return EINVAL;
+	}
+
+	SYS_SEM_LOCK(&posix_spin_lock) {
+		l = posix_get_pool_obj_unlocked(&posix_spin_pool, *lock);
+		if (l == NULL) {
+			/* not specified as part of POSIX but this is the Linux behavior */
+			ret = EINVAL;
+			SYS_SEM_LOCK_BREAK;
+		}
+
+		ret = 0;
+	}
+
+	if (ret == 0) {
+		while (!atomic_cas(l, 0, 1)) {
+			arch_nop();
+			if (wait) {
+				continue;
+			}
+			ret = EBUSY;
+			break;
+		}
+	}
+
+	return ret;
 }
 
 int pthread_spin_lock(pthread_spinlock_t *lock)
 {
-	size_t bit;
-	struct k_spinlock *l;
-
-	l = get_posix_spinlock(lock);
-	if (l == NULL) {
-		/* not specified as part of POSIX but this is the Linux behavior */
-		return EINVAL;
-	}
-
-	bit = posix_spinlock_to_offset(l);
-	posix_spinlock_key[bit] = k_spin_lock(l);
-
-	return 0;
+	return pthread_spin_lock_common(lock, true);
 }
 
 int pthread_spin_trylock(pthread_spinlock_t *lock)
 {
-	size_t bit;
-	struct k_spinlock *l;
-
-	l = get_posix_spinlock(lock);
-	if (l == NULL) {
-		/* not specified as part of POSIX but this is the Linux behavior */
-		return EINVAL;
-	}
-
-	bit = posix_spinlock_to_offset(l);
-	return k_spin_trylock(l, &posix_spinlock_key[bit]);
+	return pthread_spin_lock_common(lock, false);
 }
 
 int pthread_spin_unlock(pthread_spinlock_t *lock)
 {
-	size_t bit;
-	struct k_spinlock *l;
+	atomic_t *l;
+	int ret = EINVAL;
 
-	l = get_posix_spinlock(lock);
-	if (l == NULL) {
-		/* not specified as part of POSIX but this is the Linux behavior */
+	if (lock == NULL) {
 		return EINVAL;
 	}
 
-	bit = posix_spinlock_to_offset(l);
-	k_spin_unlock(l, posix_spinlock_key[bit]);
+	SYS_SEM_LOCK(&posix_spin_lock) {
+		l = posix_get_pool_obj_unlocked(&posix_spin_pool, *lock);
+		if (l == NULL) {
+			/* not specified as part of POSIX but this is the Linux behavior */
+			ret = EINVAL;
+			SYS_SEM_LOCK_BREAK;
+		}
 
-	return 0;
+		ret = 0;
+	}
+
+	if (ret == 0) {
+		while (!atomic_cas(l, 1, 0)) {
+			arch_nop();
+		}
+	}
+
+	return ret;
 }
