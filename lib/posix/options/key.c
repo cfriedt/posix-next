9ffd23f4b47e373ad8dbcae0e7f230c662601320
Chris Friedt <chris@fr4.co>
2025-12-21 16:07:44 -0500

    posix: threads: move impl to sys/thread.h and refactor thrd impl
    Refactor the implementation of POSIX threads out of lib/posix, and into
    lib/os as the sys/thread.h API which mainly serves to complement the
    k_thread (k_mutex, k_condvar) APIs.
    
    This change was required in order to remove POSIX from the ISO C threads
    equation and simultaneously enables us to shrink the POSIX API to the
    bare minimum required.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- lib/posix/options/key.c
+++ lib/posix/options/key.c
@@ -5,202 +5,36 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#include "posix_internal.h"
+
+#include <pthread.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-#include <pthread.h>
-#include <zephyr/sys/bitarray.h>
-#include <zephyr/sys/__assert.h>
-#include <zephyr/sys/sem.h>
+#include <zephyr/sys/thread.h>
 
-LOG_MODULE_REGISTER(pthread_key, CONFIG_PTHREAD_KEY_LOG_LEVEL);
-
-/* This is non-standard (i.e. an implementation detail) */
-#define PTHREAD_KEY_INITIALIZER (-1)
-
-SYS_SEM_DEFINE(pthread_key_lock, 1, 1);
-SYS_ELASTIPOOL_DEFINE_STATIC(posix_key_pool, sizeof(pthread_key_obj), __alignof(pthread_key_obj),
-			     CONFIG_POSIX_THREAD_KEYS_MAX, CONFIG_POSIX_THREAD_KEYS_MAX);
-
-/**
- * @brief Create a key for thread-specific data
- *
- * See IEEE 1003.1
- */
-int pthread_key_create(pthread_key_t *key,
-		void (*destructor)(void *))
+int pthread_key_create(pthread_key_t *key, void (*destructor)(void *))
 {
-	pthread_key_obj *new_key;
-
-	*key = PTHREAD_KEY_INITIALIZER;
-	new_key = posix_init_pool_obj(&posix_key_pool, &pthread_key_lock, *key, NULL);
-	if (new_key == NULL) {
-		return ENOMEM;
-	}
-
-	sys_slist_init(&(new_key->key_data_l));
-	new_key->destructor = destructor;
-	*key = (pthread_key_t)(uintptr_t)new_key;
-
-	LOG_DBG("Initialized key %p (%x)", new_key, *key);
-
-	return 0;
+	return -sys_thread_key_create((sys_thread_key_t *)key, destructor);
 }
 
-/**
- * @brief Delete a key for thread-specific data
- *
- * See IEEE 1003.1
- */
 int pthread_key_delete(pthread_key_t key)
 {
-	int ret = EINVAL;
-	pthread_key_obj *key_obj = NULL;
-	struct pthread_key_data *key_data;
-	sys_snode_t *node_l, *next_node_l;
-
-	SYS_SEM_LOCK(&pthread_key_lock) {
-		key_obj = posix_get_pool_obj_unlocked(&posix_key_pool, key);
-		if (key_obj == NULL) {
-			ret = EINVAL;
-			SYS_SEM_LOCK_BREAK;
-		}
-
-		/* Delete thread-specific elements associated with the key */
-		SYS_SLIST_FOR_EACH_NODE_SAFE(&(key_obj->key_data_l), node_l, next_node_l) {
-
-			/* Remove the object from the list key_data_l */
-			key_data = (struct pthread_key_data *)sys_slist_get(&(key_obj->key_data_l));
-
-			/* Deallocate the object's memory */
-			k_free((void *)key_data);
-			LOG_DBG("Freed key data %p for key %x in thread %x", key_data, key,
-				pthread_self());
-		}
-
-		ret = sys_elastipool_free(&posix_key_pool, key_obj);
-		__ASSERT_NO_MSG(ret == 0);
-	}
-
-	if (ret == 0) {
-		LOG_DBG("Deleted key %p (%x)", key_obj, key);
-	}
-
-	return ret;
+	return -sys_thread_key_delete((sys_thread_key_t)key);
 }
 
-/**
- * @brief Associate a thread-specific value with a key
- *
- * See IEEE 1003.1
- */
 int pthread_setspecific(pthread_key_t key, const void *value)
 {
-	pthread_key_obj *key_obj = NULL;
-	struct posix_thread *thread;
-	struct pthread_key_data *key_data;
-	sys_snode_t *node_l = NULL;
-	int retval = 0;
-
-	thread = to_posix_thread(pthread_self());
-	if (thread == NULL) {
-		return EINVAL;
-	}
-
-	/* Traverse the list of keys set by the thread, looking for key.
-	 * If the key is already in the list, re-assign its value.
-	 * Else add the key to the thread's list.
-	 */
-	SYS_SEM_LOCK(&pthread_key_lock) {
-		key_obj = posix_get_pool_obj_unlocked(&posix_key_pool, key);
-		if (key_obj == NULL) {
-			retval = EINVAL;
-			SYS_SEM_LOCK_BREAK;
-		}
-
-		SYS_SLIST_FOR_EACH_NODE(&(thread->key_list), node_l) {
-			pthread_thread_data *thread_spec_data = (pthread_thread_data *)node_l;
-
-			if (thread_spec_data->key == key_obj) {
-				/* Key is already present so associate thread specific data */
-				thread_spec_data->spec_data = (void *)value;
-				LOG_DBG("Paired key %x to value %p for thread %x", key, value,
-					pthread_self());
-				break;
-			}
-		}
-
-		retval = 0;
-		if (node_l != NULL) {
-			/* Key is already present, so we are done */
-			SYS_SEM_LOCK_BREAK;
-		}
-
-		/* Key and data need to be added */
-		key_data = k_malloc(sizeof(struct pthread_key_data));
-
-		if (key_data == NULL) {
-			LOG_DBG("Failed to allocate key data for key %x", key);
-			retval = ENOMEM;
-			SYS_SEM_LOCK_BREAK;
-		}
-
-		LOG_DBG("Allocated key data %p for key %x in thread %x", key_data, key,
-			pthread_self());
-
-		/* Associate thread specific data, initialize new key */
-		key_data->thread_data.key = key_obj;
-		key_data->thread_data.spec_data = (void *)value;
-
-		/* Append new thread key data to thread's key list */
-		sys_slist_append((&thread->key_list), (sys_snode_t *)(&key_data->thread_data));
-
-		/* Append new key data to the key object's list */
-		sys_slist_append(&(key_obj->key_data_l), (sys_snode_t *)key_data);
-
-		LOG_DBG("Paired key %x to value %p for thread %x", key, value, pthread_self());
-	}
-
-	return retval;
+	return -sys_thread_setspecific((sys_thread_key_t)key, value);
 }
 
-/**
- * @brief Get the thread-specific value associated with the key
- *
- * See IEEE 1003.1
- */
 void *pthread_getspecific(pthread_key_t key)
 {
-	pthread_key_obj *key_obj;
-	struct posix_thread *thread;
-	pthread_thread_data *thread_spec_data;
-	void *value = NULL;
-	sys_snode_t *node_l;
+	void *val = NULL;
 
-	thread = to_posix_thread(pthread_self());
-	if (thread == NULL) {
+	if (sys_thread_getspecific((sys_thread_key_t)key, &val) < 0) {
 		return NULL;
 	}
 
-	SYS_SEM_LOCK(&pthread_key_lock) {
-		key_obj = posix_get_pool_obj_unlocked(&posix_key_pool, key);
-		if (key_obj == NULL) {
-			value = NULL;
-			SYS_SEM_LOCK_BREAK;
-		}
-
-		/* Traverse the list of keys set by the thread, looking for key */
-
-		SYS_SLIST_FOR_EACH_NODE(&(thread->key_list), node_l) {
-			thread_spec_data = (pthread_thread_data *)node_l;
-			if (thread_spec_data->key == key_obj) {
-				/* Key is present, so get the set thread data */
-				value = thread_spec_data->spec_data;
-				break;
-			}
-		}
-	}
-
-	return value;
+	return val;
 }
--- lib/posix/options/mutex.c
+++ lib/posix/options/mutex.c
@@ -1,26 +1,16 @@
 /*
- * Copyright (c) 2017 Intel Corporation
- * Copyright (c) 2023 Meta
- * Copyright (c) 2025, Friedt Professional Engineering Services, Inc.
+ * Copyright (c) The Zephyr Project Contributors
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "posix_clock.h"
 #include "posix_internal.h"
 
 #include <pthread.h>
-#include <sys/types.h>
 
-#include <zephyr/init.h>
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/sys/bitarray.h>
-#include <zephyr/sys/sem.h>
-
-LOG_MODULE_REGISTER(pthread_mutex, CONFIG_PTHREAD_MUTEX_LOG_LEVEL);
-
-#define MUTEX_MAX_REC_LOCK 32767
+#include <zephyr/sys/thread.h>
+#include <zephyr/sys/timeutil.h>
 
 struct pthread_mutexattr {
 	unsigned char type: 2;
@@ -28,276 +18,73 @@ struct pthread_mutexattr {
 };
 BUILD_ASSERT(sizeof(pthread_mutexattr_t) >= sizeof(struct pthread_mutexattr));
 
-/*
- *  Default mutex attrs.
- */
-static const struct pthread_mutexattr def_attr = {
-	.type = PTHREAD_MUTEX_DEFAULT,
-};
-
-static SYS_SEM_DEFINE(posix_mutex_lock, 1, 1);
-SYS_ELASTIPOOL_DEFINE_STATIC(posix_mutex_pool, sizeof(struct k_mutex), __alignof(struct k_mutex),
-			     CONFIG_MAX_PTHREAD_MUTEX_COUNT, CONFIG_MAX_PTHREAD_MUTEX_COUNT);
-/* fixme: this should just be added to zephyr's mutex descriptor */
-static __pinned_bss uint8_t posix_mutex_type[CONFIG_MAX_PTHREAD_MUTEX_COUNT];
-
-static inline size_t posix_mutex_to_offset(struct k_mutex *m)
+int pthread_mutex_destroy(pthread_mutex_t *mu)
 {
-	/* fixme: this prevents dynamic growth with elastipool */
-	return ((uint8_t *)m - posix_mutex_pool.config->storage) /
-	       ROUND_UP(posix_mutex_pool.config->obj_size, posix_mutex_pool.config->obj_align);
+	return -sys_mutex_destroy(*(struct k_mutex **)mu);
 }
 
-static void mutex_init_pool_obj_cb(void *obj)
+static int pthread_mutexattr_to_flags(const pthread_mutexattr_t *attr, int *flags)
 {
-	int err;
-	struct k_mutex *const m = (struct k_mutex *)obj;
+	if (attr == NULL) {
+		*flags = 0;
+		return 0;
+	}
 
-	err = k_mutex_init(m);
-	__ASSERT_NO_MSG(err == 0);
+	struct pthread_mutexattr *const a = (struct pthread_mutexattr *)attr;
+
+	if (!a->initialized) {
+		return -EINVAL;
+	}
+
+	switch (a->type) {
+	case PTHREAD_MUTEX_DEFAULT:
+	case PTHREAD_MUTEX_NORMAL:
+	case PTHREAD_MUTEX_ROBUST:
+		break;
+	case PTHREAD_MUTEX_RECURSIVE:
+		*flags = SYS_MUTEX_RECURSIVE;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
 }
 
-struct k_mutex *to_posix_mutex(pthread_mutex_t *mu)
+int pthread_mutex_init(pthread_mutex_t *mu, const pthread_mutexattr_t *attr)
 {
-	return posix_init_pool_obj(&posix_mutex_pool, &posix_mutex_lock, *mu,
-				   mutex_init_pool_obj_cb);
-}
+	int flags = 0;
 
-static int acquire_mutex(pthread_mutex_t *mu, k_timeout_t timeout)
-{
-	int type = -1;
-	size_t bit = -1;
-	int ret = EINVAL;
-	size_t lock_count = -1;
-	struct k_mutex *m = NULL;
-	struct k_thread *owner = NULL;
-
-	SYS_SEM_LOCK(&posix_mutex_lock) {
-		m = posix_init_pool_obj_unlocked(&posix_mutex_pool, *mu, mutex_init_pool_obj_cb);
-		if (m == NULL) {
-			ret = EINVAL;
-			SYS_SEM_LOCK_BREAK;
-		}
-		*mu = (pthread_mutex_t)(uintptr_t)m;
-
-		ret = 0;
-		bit = posix_mutex_to_offset(m);
-		type = posix_mutex_type[bit];
-		owner = m->owner;
-		lock_count = m->lock_count;
-
-		LOG_DBG("Locking mutex %p (bit %zu, type %d) with timeout %" PRIx64, m, bit, type,
-			(int64_t)timeout.ticks);
-	}
-
-	if (ret != 0) {
-		goto handle_error;
-	}
-
-	if (owner == k_current_get()) {
-		switch (type) {
-		case PTHREAD_MUTEX_DEFAULT:
-		case PTHREAD_MUTEX_NORMAL:
-			if (K_TIMEOUT_EQ(timeout, K_NO_WAIT)) {
-				LOG_DBG("Timeout locking mutex %p", m);
-				ret = EBUSY;
-				break;
-			}
-			/* On most POSIX systems, this usually results in an infinite loop */
-			LOG_DBG("Attempt to relock non-recursive mutex %p", m);
-			do {
-				(void)k_sleep(K_FOREVER);
-			} while (true);
-			CODE_UNREACHABLE;
-			break;
-		case PTHREAD_MUTEX_RECURSIVE:
-			if (lock_count >= MUTEX_MAX_REC_LOCK) {
-				LOG_DBG("Mutex %p locked recursively too many times", m);
-				ret = EAGAIN;
-			}
-			break;
-		case PTHREAD_MUTEX_ERRORCHECK:
-			LOG_DBG("Attempt to recursively lock non-recursive mutex %p", m);
-			ret = EDEADLK;
-			break;
-		default:
-			__ASSERT(false, "invalid pthread type %d", type);
-			ret = EINVAL;
-			break;
-		}
-	}
-
-	if (ret == 0) {
-		ret = k_mutex_lock(m, timeout);
-		if (ret == -EAGAIN) {
-			LOG_DBG("Timeout locking mutex %p", m);
-			/*
-			 * special quirk - k_mutex_lock() returns EAGAIN if a timeout occurs, but
-			 * for pthreads, that means something different
-			 */
-			ret = ETIMEDOUT;
-		}
-	}
-
-handle_error:
-	if (ret < 0) {
-		LOG_DBG("k_mutex_unlock() failed: %d", ret);
-		ret = -ret;
-	}
-
-	if (ret == 0) {
-		LOG_DBG("Locked mutex %p", m);
-	}
-
-	return ret;
-}
-
-/**
- * @brief Lock POSIX mutex with non-blocking call.
- *
- * See IEEE 1003.1
- */
-int pthread_mutex_trylock(pthread_mutex_t *m)
-{
-	return acquire_mutex(m, K_NO_WAIT);
-}
-
-/**
- * @brief Lock POSIX mutex with timeout.
- *
- *
- * See IEEE 1003.1
- */
-int pthread_mutex_timedlock(pthread_mutex_t *m,
-			    const struct timespec *abstime)
-{
-	if ((abstime == NULL) || !timespec_is_valid(abstime)) {
-		LOG_DBG("%s is invalid", "abstime");
+	if (pthread_mutexattr_to_flags(attr, &flags) < 0) {
 		return EINVAL;
 	}
 
-	return acquire_mutex(m, K_MSEC(timespec_to_timeoutms(CLOCK_REALTIME, abstime)));
+	return -sys_mutex_init((struct k_mutex **)mu, flags);
 }
 
-/**
- * @brief Initialize POSIX mutex.
- *
- * See IEEE 1003.1
- */
-int pthread_mutex_init(pthread_mutex_t *mu, const pthread_mutexattr_t *_attr)
-{
-	size_t bit;
-	int type = -1;
-	int ret = ENOMEM;
-	struct k_mutex *m;
-	const struct pthread_mutexattr *attr = (const struct pthread_mutexattr *)_attr;
-
-	*mu = PTHREAD_MUTEX_INITIALIZER;
-
-	SYS_SEM_LOCK(&posix_mutex_lock) {
-		m = posix_init_pool_obj_unlocked(&posix_mutex_pool, *mu, mutex_init_pool_obj_cb);
-		if (m == NULL) {
-			ret = ENOMEM;
-			SYS_SEM_LOCK_BREAK;
-		}
-		*mu = (pthread_mutex_t)(uintptr_t)m;
-
-		bit = posix_mutex_to_offset(m);
-		if (attr == NULL) {
-			posix_mutex_type[bit] = def_attr.type;
-		} else {
-			posix_mutex_type[bit] = attr->type;
-		}
-
-		type = posix_mutex_type[bit];
-		ret = 0;
-	}
-
-	if (ret == 0) {
-		LOG_DBG("Initialized mutex %p, bit %zu, type %d", m, bit, type);
-	}
-
-	return ret;
-}
-
-/**
- * @brief Lock POSIX mutex with blocking call.
- *
- * See IEEE 1003.1
- */
 int pthread_mutex_lock(pthread_mutex_t *m)
 {
-	return acquire_mutex(m, K_FOREVER);
+	return -k_mutex_lock(*(struct k_mutex **)m, K_FOREVER);
 }
 
-/**
- * @brief Unlock POSIX mutex.
- *
- * See IEEE 1003.1
- */
+int pthread_mutex_timedlock(pthread_mutex_t *m,
+			    const struct timespec *abstime)
+{
+	return -k_mutex_lock(*(struct k_mutex **)m,
+			     sys_timepoint_timeout(timespec_to_timepoint(abstime)));
+}
+
+int pthread_mutex_trylock(pthread_mutex_t *m)
+{
+	return -k_mutex_lock(*(struct k_mutex **)m, K_NO_WAIT);
+}
+
 int pthread_mutex_unlock(pthread_mutex_t *mu)
 {
-	int ret = EINVAL;
-	struct k_mutex *m;
-
-	SYS_SEM_LOCK(&posix_mutex_lock) {
-		m = posix_get_pool_obj_unlocked(&posix_mutex_pool, *mu);
-		if (m == NULL) {
-			ret = EINVAL;
-			SYS_SEM_LOCK_BREAK;
-		}
-
-		ret = k_mutex_unlock(m);
-		if (ret < 0) {
-			LOG_DBG("k_mutex_unlock() failed: %d", ret);
-			ret = -ret;
-			SYS_SEM_LOCK_BREAK;
-		}
-
-		ret = 0;
-	}
-
-	if (ret == 0) {
-		LOG_DBG("Unlocked mutex %p", m);
-	}
-
-	return ret;
-}
-
-/**
- * @brief Destroy POSIX mutex.
- *
- * See IEEE 1003.1
- */
-int pthread_mutex_destroy(pthread_mutex_t *mu)
-{
-	int ret = EINVAL;
-	struct k_mutex *m;
-
-	SYS_SEM_LOCK(&posix_mutex_lock) {
-		m = posix_get_pool_obj_unlocked(&posix_mutex_pool, *mu);
-		if (m == NULL) {
-			ret = EINVAL;
-			SYS_SEM_LOCK_BREAK;
-		}
-
-		ret = sys_elastipool_free(&posix_mutex_pool, m);
-		__ASSERT_NO_MSG(ret == 0);
-	}
-
-	if (ret == 0) {
-		LOG_DBG("Destroyed mutex %p", m);
-	}
-
-	return ret;
+	return -k_mutex_unlock(*(struct k_mutex **)mu);
 }
 
 #if defined(_POSIX_THREAD_PRIO_PROTECT)
-/**
- * @brief Read protocol attribute for mutex.
- *
- * See IEEE 1003.1
- */
 int pthread_mutexattr_getprotocol(const pthread_mutexattr_t *attr,
 				  int *protocol)
 {
@@ -309,11 +96,6 @@ int pthread_mutexattr_getprotocol(const pthread_mutexattr_t *attr,
 	return 0;
 }
 
-/**
- * @brief Set protocol attribute for mutex.
- *
- * See IEEE 1003.1
- */
 int pthread_mutexattr_setprotocol(pthread_mutexattr_t *attr, int protocol)
 {
 	if (attr == NULL) {
@@ -341,8 +123,10 @@ int pthread_mutexattr_init(pthread_mutexattr_t *attr)
 		return EINVAL;
 	}
 
-	a->type = PTHREAD_MUTEX_DEFAULT;
-	a->initialized = true;
+	*a = (struct pthread_mutexattr){
+		.type = PTHREAD_MUTEX_DEFAULT,
+		.initialized = true,
+	};
 
 	return 0;
 }
@@ -360,11 +144,6 @@ int pthread_mutexattr_destroy(pthread_mutexattr_t *attr)
 	return 0;
 }
 
-/**
- * @brief Read type attribute for mutex.
- *
- * See IEEE 1003.1
- */
 int pthread_mutexattr_gettype(const pthread_mutexattr_t *attr, int *type)
 {
 	const struct pthread_mutexattr *a = (const struct pthread_mutexattr *)attr;
@@ -378,11 +157,6 @@ int pthread_mutexattr_gettype(const pthread_mutexattr_t *attr, int *type)
 	return 0;
 }
 
-/**
- * @brief Set type attribute for mutex.
- *
- * See IEEE 1003.1
- */
 int pthread_mutexattr_settype(pthread_mutexattr_t *attr, int type)
 {
 	struct pthread_mutexattr *const a = (struct pthread_mutexattr *)attr;
@@ -403,7 +177,7 @@ int pthread_mutexattr_settype(pthread_mutexattr_t *attr, int type)
 	}
 }
 
-#ifdef CONFIG_POSIX_THREAD_PRIO_PROTECT
+#if defined(_POSIX_THREAD_PRIO_PROTECT)
 int pthread_mutex_getprioceiling(const pthread_mutex_t *mutex, int *prioceiling)
 {
 	ARG_UNUSED(mutex);
@@ -437,22 +211,4 @@ int pthread_mutexattr_setprioceiling(pthread_mutexattr_t *attr, int prioceiling)
 	return ENOSYS;
 }
 
-#endif /* CONFIG_POSIX_THREAD_PRIO_PROTECT */
-
-__boot_func
-static int pthread_mutex_pool_init(void)
-{
-	int err;
-	size_t i;
-
-	for (i = 0; i < CONFIG_MAX_PTHREAD_MUTEX_COUNT; ++i) {
-		struct k_mutex *const m = (struct k_mutex *)posix_mutex_pool.config->storage;
-
-		err = k_mutex_init(m);
-		__ASSERT_NO_MSG(err == 0);
-		posix_mutex_type[i] = PTHREAD_MUTEX_DEFAULT;
-	}
-
-	return 0;
-}
-SYS_INIT(pthread_mutex_pool_init, PRE_KERNEL_1, 0);
+#endif
