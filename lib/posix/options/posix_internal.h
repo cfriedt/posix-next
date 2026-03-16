74565c075d1f2e11de0cb34f675264668a394c69
Chris Friedt <chris@fr4.co>
2026-03-16 07:55:29 -0400

    posix: pthread: transition to native Zephyr apis
    This change unifies the reference implemention of POSIX threads with
    Zephyr's native threading API. In other words, these are each equivalent
    now:
    
    - pthread_t <=> k_thread
    - pthread_mutex_t <=> k_mutex
    - pthread_cond_t <=> k_condvar
    
    Additionally, automatic thread stack allocation is now achieved via the
    sys_thread_create() rather than via pthread_attr_init(), which
    drastically simplifies thread stack management and also fixes upstream
    bug #103856.
    
    A special note should be made that POSIX threads now also inherently
    support both kernel and userspace permissions.
    
    With this transition, the C11 (and by extension C++11) threading APIs
    can also directly use native Zephyr threading APIs instead of leaning on
    POSIX support. Implementing the mutual dependency strategy of resolving
    dependency cycles here.
    
    https://docs.zephyrproject.org/4.4.0/services/portability/posix/\
    implementation/index.html
    
    This is effectively the beginning of a Native POSIX Thread Library (NPTL)
    for Zephyr. It means that there is virtually zero overhead to use the
    POSIX threads API on top of Zephyr's native threading API. Likewise,
    the C11 (and by extension C++11) thread APIs also should have
    approximately zero overhead on top of Zephyr's native threading APIs.
    
    Also in this commit:
    
    Implement `pthread_cond_clockwait()`, which is a relatively new variant
    of the `pthread_cond_*wait()` functions that allows the caller to
    specify the clock_id against which to measure the absolute time until
    returning.
    
    This is implemented on the Zephyr side using `k_condvar_clockwait()`.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- lib/posix/options/posix_internal.h
+++ lib/posix/options/posix_internal.h
@@ -20,56 +20,6 @@
 
 #define POSIX_OBJ_INITIALIZER (-1)
 
-/*
- * Bit used to mark a pthread object as initialized. Initialization status is
- * verified (against internal status) in lock / unlock / destroy functions.
- */
-#define PTHREAD_OBJ_MASK_INIT 0x80000000
-
-struct __packed posix_thread_attr {
-	void *stack;
-	/* the following two bitfields should combine to be 32-bits in size */
-	uint32_t stacksize: CONFIG_POSIX_PTHREAD_ATTR_STACKSIZE_BITS;
-	uint16_t guardsize: CONFIG_POSIX_PTHREAD_ATTR_GUARDSIZE_BITS;
-	int8_t priority;
-	uint8_t schedpolicy: 2;
-	bool contentionscope: 1;
-	bool inheritsched: 1;
-	union {
-		bool caller_destroys: 1;
-		bool initialized: 1;
-	};
-	bool cancelpending: 1;
-	bool cancelstate: 1;
-	bool canceltype: 1;
-	bool detachstate: 1;
-};
-
-struct posix_thread {
-	struct k_thread thread;
-
-	/* List nodes for pthread_cleanup_push() / pthread_cleanup_pop() */
-	sys_slist_t cleanup_list;
-
-	/* List node for ready_q, run_q, or done_q */
-	sys_dnode_t q_node;
-
-	/* List of keys that thread has called pthread_setspecific() on */
-	sys_slist_t key_list;
-
-	/* pthread_attr_t */
-	struct posix_thread_attr attr;
-
-	/* Exit status */
-	void *retval;
-
-	/* Signal mask */
-	sigset_t sigset;
-
-	/* Queue ID (internal-only) */
-	uint8_t qid;
-};
-
 struct posix_condattr {
 	/* leaves room for CLOCK_REALTIME (1, default) and CLOCK_MONOTONIC (4) */
 	unsigned char clock: 3;
@@ -83,44 +33,6 @@ struct posix_cond {
 	struct k_condvar condvar;
 	struct posix_condattr attr;
 };
-
-typedef struct pthread_key_obj {
-	/* List of pthread_key_data objects that contain thread
-	 * specific data for the key
-	 */
-	sys_slist_t key_data_l;
-
-	/* Optional destructor that is passed to pthread_key_create() */
-	void (*destructor)(void *value);
-} pthread_key_obj;
-
-typedef struct pthread_thread_data {
-	sys_snode_t node;
-
-	/* Key and thread specific data passed to pthread_setspecific() */
-	pthread_key_obj *key;
-	void *spec_data;
-} pthread_thread_data;
-
-struct pthread_key_data {
-	sys_snode_t node;
-	pthread_thread_data thread_data;
-};
-
-static inline bool is_pthread_obj_initialized(uint32_t obj)
-{
-	return (obj & PTHREAD_OBJ_MASK_INIT) != 0;
-}
-
-static inline uint32_t mark_pthread_obj_initialized(uint32_t obj)
-{
-	return obj | PTHREAD_OBJ_MASK_INIT;
-}
-
-static inline uint32_t mark_pthread_obj_uninitialized(uint32_t obj)
-{
-	return obj & ~PTHREAD_OBJ_MASK_INIT;
-}
 
 /* get a pointer to a pool object that has already been allocated using handle */
 void *posix_get_pool_obj_unlocked(const struct sys_elastipool *pool, uint32_t handle);
@@ -159,5 +71,86 @@ struct k_mutex *to_posix_mutex(pthread_mutex_t *mu);
 
 int posix_to_zephyr_priority(int priority, int policy);
 int zephyr_to_posix_priority(int priority, int *policy);
+
+BUILD_ASSERT((sizeof(void *) == sizeof(pthread_barrier_t)) ||
+		     (sizeof(void *) == 2 * sizeof(pthread_barrier_t)),
+	     "unsupported pthread_barrier_t size");
+BUILD_ASSERT((sizeof(void *) == sizeof(pthread_cond_t)) ||
+		     (sizeof(void *) == 2 * sizeof(pthread_cond_t)),
+	     "unsupported pthread_cond_t size");
+BUILD_ASSERT((sizeof(void *) == sizeof(pthread_key_t)) ||
+		     (sizeof(void *) == 2 * sizeof(pthread_key_t)),
+	     "unsupported pthread_key_t size");
+BUILD_ASSERT((sizeof(void *) == sizeof(pthread_mutex_t)) ||
+		     (sizeof(void *) == 2 * sizeof(pthread_mutex_t)),
+	     "unsupported pthread_mutex_t size");
+
+/* FIXME: Need to adjust the toolchain so that pthread_t, pthread_mutex_t, pthread_cond_t,
+ * pthread_key_t, etc are the same size of uintptr_t (i.e. void *) */
+static inline void *posix_to_kernel_object(void *input, size_t size, void *ref)
+{
+	void *output = NULL;
+
+	if (sizeof(void *) == size) {
+		output = (void *)*((uintptr_t *)input);
+	} else if ((sizeof(void *) == 2 * size) && (sizeof(uint32_t) == size)) {
+		output = (void *)(uintptr_t)(((uintptr_t)ref & GENMASK64(63, 32)) |
+					     *(uint32_t *)input);
+	}
+
+	return output;
+}
+
+static inline uintptr_t posix_from_kernel_object(void *input, size_t size)
+{
+	uintptr_t output = 0;
+
+	if (sizeof(void *) == size) {
+		output = (uintptr_t)input;
+	} else if (sizeof(void *) == 2 * size) {
+		output = (uintptr_t)input & GENMASK64(31, 0);
+	}
+
+	return output;
+}
+
+static inline struct k_mutex *to_k_mutex(const pthread_mutex_t *mu)
+{
+	extern struct k_mutex sys_mutex_pool[];
+
+	return (struct k_mutex *)posix_to_kernel_object((void *)mu, sizeof(pthread_mutex_t),
+							sys_mutex_pool);
+}
+
+static inline pthread_mutex_t to_pthread_mutex(const struct k_mutex *kmu)
+{
+	return (pthread_mutex_t)posix_from_kernel_object((void *)kmu, sizeof(pthread_mutex_t));
+}
+
+static inline struct k_condvar *to_k_condvar(const pthread_cond_t *cv)
+{
+	extern struct k_condvar sys_condvar_pool[];
+
+	return (struct k_condvar *)posix_to_kernel_object((void *)cv, sizeof(pthread_cond_t),
+							  sys_condvar_pool);
+}
+
+static inline pthread_cond_t to_pthread_cond(const struct k_condvar *kcv)
+{
+	return (pthread_cond_t)posix_from_kernel_object((void *)kcv, sizeof(pthread_cond_t));
+}
+
+static inline struct k_thread *to_k_thread(const pthread_t *th)
+{
+	extern struct k_thread sys_thread_pool[];
+
+	return (struct k_thread *)posix_to_kernel_object((void *)th, sizeof(pthread_t),
+							 sys_thread_pool);
+}
+
+static inline pthread_t to_pthread_thread(const struct k_thread *kth)
+{
+	return (pthread_t)posix_from_kernel_object((void *)kth, sizeof(pthread_t));
+}
 
 #endif
