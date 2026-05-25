8d921df8ab7cb35faa29719c40b783f2dc00d2a5
Chris Friedt <chris@fr4.co>
2026-05-24 21:50:47 -0400

    doc: doxygenate networking and system info headers
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- include/zephyr/posix/netinet/in.h
+++ include/zephyr/posix/netinet/in.h
@@ -3,6 +3,22 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+/**
+ * @file
+ * @brief Internet address types (<netinet/in.h>)
+ *
+ * Provides the fundamental Internet address and port types used across the
+ * BSD socket API and the IP protocol family headers.
+ *
+ * @see <a href="https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/netinet_in.h.html">
+ *      POSIX.1-2017 &lt;netinet/in.h&gt;</a>
+ *
+ * @defgroup posix_netinet_in Internet Address Types
+ * @ingroup posix_option_group_networking
+ * @{
+ */
+
 #ifndef ZEPHYR_INCLUDE_POSIX_NETINET_IN_H_
 #define ZEPHYR_INCLUDE_POSIX_NETINET_IN_H_
 
@@ -14,8 +30,13 @@
 extern "C" {
 #endif
 
+/** @brief Unsigned 16-bit Internet port number. */
 typedef uint16_t in_port_t;
+
+/** @brief Unsigned 32-bit IPv4 address. */
 typedef uint32_t in_addr_t;
+
+/** @} */
 
 #ifdef __cplusplus
 }
--- include/zephyr/posix/sys/confstr.h
+++ include/zephyr/posix/sys/confstr.h
@@ -3,6 +3,22 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+/**
+ * @file
+ * @brief POSIX configuration string constants (<sys/confstr.h>)
+ *
+ * Defines the @c _CS_* name constants used as the first argument to
+ * confstr() to query implementation-defined configuration strings.
+ *
+ * @see <a href="https://pubs.opengroup.org/onlinepubs/9699919799/functions/confstr.html">
+ *      POSIX.1-2017 confstr()</a>
+ *
+ * @defgroup posix_confstr POSIX Configuration Strings
+ * @ingroup posix_option_group_single_process
+ * @{
+ */
+
 #ifndef ZEPHYR_INCLUDE_POSIX_SYS_CONFSTR_H_
 #define ZEPHYR_INCLUDE_POSIX_SYS_CONFSTR_H_
 
@@ -10,39 +26,42 @@
 extern "C" {
 #endif
 
+/** @brief confstr() name constants. */
 enum {
-	_CS_PATH,
-	_CS_POSIX_V7_ILP32_OFF32_CFLAGS,
-	_CS_POSIX_V7_ILP32_OFF32_LDFLAGS,
-	_CS_POSIX_V7_ILP32_OFF32_LIBS,
-	_CS_POSIX_V7_ILP32_OFFBIG_CFLAGS,
-	_CS_POSIX_V7_ILP32_OFFBIG_LDFLAGS,
-	_CS_POSIX_V7_ILP32_OFFBIG_LIBS,
-	_CS_POSIX_V7_LP64_OFF64_CFLAGS,
-	_CS_POSIX_V7_LP64_OFF64_LDFLAGS,
-	_CS_POSIX_V7_LP64_OFF64_LIBS,
-	_CS_POSIX_V7_LPBIG_OFFBIG_CFLAGS,
-	_CS_POSIX_V7_LPBIG_OFFBIG_LDFLAGS,
-	_CS_POSIX_V7_LPBIG_OFFBIG_LIBS,
-	_CS_POSIX_V7_THREADS_CFLAGS,
-	_CS_POSIX_V7_THREADS_LDFLAGS,
-	_CS_POSIX_V7_WIDTH_RESTRICTED_ENVS,
-	_CS_V7_ENV,
-	_CS_POSIX_V6_ILP32_OFF32_CFLAGS,
-	_CS_POSIX_V6_ILP32_OFF32_LDFLAGS,
-	_CS_POSIX_V6_ILP32_OFF32_LIBS,
-	_CS_POSIX_V6_ILP32_OFFBIG_CFLAGS,
-	_CS_POSIX_V6_ILP32_OFFBIG_LDFLAGS,
-	_CS_POSIX_V6_ILP32_OFFBIG_LIBS,
-	_CS_POSIX_V6_LP64_OFF64_CFLAGS,
-	_CS_POSIX_V6_LP64_OFF64_LDFLAGS,
-	_CS_POSIX_V6_LP64_OFF64_LIBS,
-	_CS_POSIX_V6_LPBIG_OFFBIG_CFLAGS,
-	_CS_POSIX_V6_LPBIG_OFFBIG_LDFLAGS,
-	_CS_POSIX_V6_LPBIG_OFFBIG_LIBS,
-	_CS_POSIX_V6_WIDTH_RESTRICTED_ENVS,
-	_CS_V6_ENV,
+	_CS_PATH,                            /**< Default value of PATH. */
+	_CS_POSIX_V7_ILP32_OFF32_CFLAGS,    /**< Compilation flags for ILP32/OFF32 (POSIX.1-2008). */
+	_CS_POSIX_V7_ILP32_OFF32_LDFLAGS,   /**< Linker flags for ILP32/OFF32 (POSIX.1-2008). */
+	_CS_POSIX_V7_ILP32_OFF32_LIBS,      /**< Libraries for ILP32/OFF32 (POSIX.1-2008). */
+	_CS_POSIX_V7_ILP32_OFFBIG_CFLAGS,   /**< Compilation flags for ILP32/OFFBIG (POSIX.1-2008). */
+	_CS_POSIX_V7_ILP32_OFFBIG_LDFLAGS,  /**< Linker flags for ILP32/OFFBIG (POSIX.1-2008). */
+	_CS_POSIX_V7_ILP32_OFFBIG_LIBS,     /**< Libraries for ILP32/OFFBIG (POSIX.1-2008). */
+	_CS_POSIX_V7_LP64_OFF64_CFLAGS,     /**< Compilation flags for LP64/OFF64 (POSIX.1-2008). */
+	_CS_POSIX_V7_LP64_OFF64_LDFLAGS,    /**< Linker flags for LP64/OFF64 (POSIX.1-2008). */
+	_CS_POSIX_V7_LP64_OFF64_LIBS,       /**< Libraries for LP64/OFF64 (POSIX.1-2008). */
+	_CS_POSIX_V7_LPBIG_OFFBIG_CFLAGS,   /**< Compilation flags for LPBIG/OFFBIG (POSIX.1-2008). */
+	_CS_POSIX_V7_LPBIG_OFFBIG_LDFLAGS,  /**< Linker flags for LPBIG/OFFBIG (POSIX.1-2008). */
+	_CS_POSIX_V7_LPBIG_OFFBIG_LIBS,     /**< Libraries for LPBIG/OFFBIG (POSIX.1-2008). */
+	_CS_POSIX_V7_THREADS_CFLAGS,        /**< Compilation flags for threads (POSIX.1-2008). */
+	_CS_POSIX_V7_THREADS_LDFLAGS,       /**< Linker flags for threads (POSIX.1-2008). */
+	_CS_POSIX_V7_WIDTH_RESTRICTED_ENVS, /**< Environments with width restrictions (POSIX.1-2008). */
+	_CS_V7_ENV,                          /**< Environments with width restrictions (alias). */
+	_CS_POSIX_V6_ILP32_OFF32_CFLAGS,    /**< Compilation flags for ILP32/OFF32 (POSIX.1-2001). */
+	_CS_POSIX_V6_ILP32_OFF32_LDFLAGS,   /**< Linker flags for ILP32/OFF32 (POSIX.1-2001). */
+	_CS_POSIX_V6_ILP32_OFF32_LIBS,      /**< Libraries for ILP32/OFF32 (POSIX.1-2001). */
+	_CS_POSIX_V6_ILP32_OFFBIG_CFLAGS,   /**< Compilation flags for ILP32/OFFBIG (POSIX.1-2001). */
+	_CS_POSIX_V6_ILP32_OFFBIG_LDFLAGS,  /**< Linker flags for ILP32/OFFBIG (POSIX.1-2001). */
+	_CS_POSIX_V6_ILP32_OFFBIG_LIBS,     /**< Libraries for ILP32/OFFBIG (POSIX.1-2001). */
+	_CS_POSIX_V6_LP64_OFF64_CFLAGS,     /**< Compilation flags for LP64/OFF64 (POSIX.1-2001). */
+	_CS_POSIX_V6_LP64_OFF64_LDFLAGS,    /**< Linker flags for LP64/OFF64 (POSIX.1-2001). */
+	_CS_POSIX_V6_LP64_OFF64_LIBS,       /**< Libraries for LP64/OFF64 (POSIX.1-2001). */
+	_CS_POSIX_V6_LPBIG_OFFBIG_CFLAGS,   /**< Compilation flags for LPBIG/OFFBIG (POSIX.1-2001). */
+	_CS_POSIX_V6_LPBIG_OFFBIG_LDFLAGS,  /**< Linker flags for LPBIG/OFFBIG (POSIX.1-2001). */
+	_CS_POSIX_V6_LPBIG_OFFBIG_LIBS,     /**< Libraries for LPBIG/OFFBIG (POSIX.1-2001). */
+	_CS_POSIX_V6_WIDTH_RESTRICTED_ENVS, /**< Environments with width restrictions (POSIX.1-2001). */
+	_CS_V6_ENV,                          /**< Environments with width restrictions (alias). */
 };
+
+/** @} */
 
 #ifdef __cplusplus
 }
