ac0e408a65155554f9806698d463302155ef5250
Chris Friedt <chris@fr4.co>
2026-05-24 21:50:47 -0400

    doc: doxygenate networking and system info headers
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- include/zephyr/posix/netinet/tcp.h
+++ include/zephyr/posix/netinet/tcp.h
@@ -3,6 +3,22 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+/**
+ * @file
+ * @brief TCP definitions (<netinet/tcp.h>)
+ *
+ * TCP socket options and constants are defined by the underlying Zephyr
+ * networking stack.  This header pulls them in via @c <zephyr/net/socket.h>.
+ *
+ * @see <a href="https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/netinet_tcp.h.html">
+ *      POSIX.1-2017 &lt;netinet/tcp.h&gt;</a>
+ *
+ * @defgroup posix_netinet_tcp TCP Definitions
+ * @ingroup posix_option_group_networking
+ * @{
+ */
+
 #ifndef ZEPHYR_INCLUDE_POSIX_NETINET_TCP_H_
 #define ZEPHYR_INCLUDE_POSIX_NETINET_TCP_H_
 
@@ -11,6 +27,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/** @} */
 
 #ifdef __cplusplus
 }
