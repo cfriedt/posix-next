239e340ec38539c8a059be4eb277151ca8658756
Chris Friedt <chris@fr4.co>
2026-06-26 09:07:13 -0400

    posix: threads_base: complete option group
    Ensure that the threads_base testsuite is compatible with Linux
    and that it is complete.
    
    Update docs to reflect that.
    
    pthread_atfork() now registers and succeeds rather than failing with
    ENOSYS: fork() does not exist, so the registered handlers can never
    run, and ENOSYS is not among the errors POSIX permits here.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

--- tests/posix/threads_base/src/cond.c
+++ tests/posix/threads_base/src/cond.c
@@ -4,9 +4,32 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <errno.h>
 #include <pthread.h>
+#include <time.h>
 
+#include <zephyr/kernel.h>
+#include <zephyr/sys/timeutil.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/ztest.h>
+
+#include "../../common/linux_compat_test.h"
+#include "_main.h"
+
+#define TIMEDWAIT_TIMEOUT_MS       200
+#define TIMEDWAIT_TIMEOUT_DELAY_MS 100
+
+BUILD_ASSERT(TIMEDWAIT_TIMEOUT_DELAY_MS >= 100, "TIMEDWAIT_TIMEOUT_DELAY_MS too small");
+BUILD_ASSERT(TIMEDWAIT_TIMEOUT_MS >= 2 * TIMEDWAIT_TIMEOUT_DELAY_MS,
+	     "TIMEDWAIT_TIMEOUT_MS too small");
+
+static inline void timespec_add_ms(struct timespec *ts, uint32_t ms)
+{
+	struct timespec addend;
+
+	timespec_from_timeout(K_MSEC(ms), &addend);
+	timespec_add(ts, &addend);
+}
 
 /**
  * @brief Test to demonstrate limited condition variable resources
@@ -14,8 +37,9 @@
  * @details Exactly SYS_THREAD_CONDVAR_MIN can be in use at once (when heap allocation is
  * unavailable).
  */
-ZTEST(cond, test_cond_resource_exhausted)
+ZTEST(posix_threads_base, test_cond_resource_exhausted)
 {
+	posix_test_skip_if_native_libc();
 	size_t i;
 	pthread_cond_t m[SYS_THREAD_CONDVAR_MIN + 1];
 
@@ -44,8 +68,9 @@ ZTEST(cond, test_cond_resource_exhausted)
  *
  * @details Demonstrate that condition variables may be used over and over again.
  */
-ZTEST(cond, test_cond_resource_leak)
+ZTEST(posix_threads_base, test_cond_resource_leak)
 {
+	posix_test_skip_if_native_libc();
 	pthread_cond_t cond;
 
 	for (size_t i = 0; i < 2 * SYS_THREAD_CONDVAR_MIN; ++i) {
@@ -54,7 +79,7 @@ ZTEST(cond, test_cond_resource_leak)
 	}
 }
 
-ZTEST(cond, test_pthread_condattr)
+static void test_pthread_condattr(void)
 {
 	pthread_condattr_t att = {0};
 
@@ -63,10 +88,9 @@ ZTEST(cond, test_pthread_condattr)
 	zassert_ok(pthread_condattr_destroy(&att));
 }
 
-/**
- * @brief Test pthread_cond_init() with a pre-existing initialized attribute.
- */
-ZTEST(cond, test_cond_init_existing_initialized_condattr)
+ZTEST_THREADS_BASE(test_pthread_condattr);
+
+static void test_cond_init_existing_initialized_condattr(void)
 {
 	pthread_cond_t cond;
 	pthread_condattr_t att = {0};
@@ -79,7 +103,9 @@ ZTEST(cond, test_cond_init_existing_initialized_condattr)
 	zassert_ok(pthread_condattr_destroy(&att));
 }
 
-ZTEST(posix_threads_base, test_cond_broadcast_static_init_pthread_cond_t)
+ZTEST_THREADS_BASE(test_cond_init_existing_initialized_condattr);
+
+static void test_cond_broadcast_static_init_pthread_cond_t(void)
 {
 	pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
@@ -87,7 +113,9 @@ ZTEST(posix_threads_base, test_cond_broadcast_static_init_pthread_cond_t)
 	zassert_ok(pthread_cond_destroy(&cond));
 }
 
-ZTEST(posix_threads_base, test_cond_signal_static_init_pthread_cond_t)
+ZTEST_THREADS_BASE(test_cond_broadcast_static_init_pthread_cond_t);
+
+static void test_cond_signal_static_init_pthread_cond_t(void)
 {
 	pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
@@ -95,4 +123,101 @@ ZTEST(posix_threads_base, test_cond_signal_static_init_pthread_cond_t)
 	zassert_ok(pthread_cond_destroy(&cond));
 }
 
-ZTEST_SUITE(cond, NULL, NULL, NULL, NULL, NULL);
+ZTEST_THREADS_BASE(test_cond_signal_static_init_pthread_cond_t);
+
+static ZTEST_BMEM pthread_mutex_t cond_wait_mtx;
+static ZTEST_BMEM pthread_cond_t cond_wait_cv;
+static ZTEST_BMEM bool cond_wait_done;
+
+static void *cond_wait_fn(void *arg)
+{
+	ARG_UNUSED(arg);
+
+	zassert_ok(pthread_mutex_lock(&cond_wait_mtx));
+	while (!cond_wait_done) {
+		zassert_ok(pthread_cond_wait(&cond_wait_cv, &cond_wait_mtx));
+	}
+	zassert_ok(pthread_mutex_unlock(&cond_wait_mtx));
+
+	return NULL;
+}
+
+static void test_pthread_cond_wait(void)
+{
+	pthread_t th;
+
+	posix_test_skip_if_native_libc();
+
+	cond_wait_done = false;
+	zassert_ok(pthread_mutex_init(&cond_wait_mtx, NULL));
+	zassert_ok(pthread_cond_init(&cond_wait_cv, NULL));
+
+	zassert_ok(pthread_create(&th, NULL, cond_wait_fn, NULL));
+
+	k_msleep(TIMEDWAIT_TIMEOUT_DELAY_MS);
+
+	zassert_ok(pthread_mutex_lock(&cond_wait_mtx));
+	cond_wait_done = true;
+	zassert_ok(pthread_cond_signal(&cond_wait_cv));
+	zassert_ok(pthread_mutex_unlock(&cond_wait_mtx));
+
+	zassert_ok(pthread_join(th, NULL));
+
+	zassert_ok(pthread_cond_destroy(&cond_wait_cv));
+	zassert_ok(pthread_mutex_destroy(&cond_wait_mtx));
+}
+
+ZTEST_THREADS_BASE(test_pthread_cond_wait);
+
+static void *cond_timedwait_fn(void *arg)
+{
+	int ret;
+	struct timespec time_point;
+	pthread_mutex_t *mtx = (pthread_mutex_t *)arg;
+
+	zassume_ok(clock_gettime(CLOCK_REALTIME, &time_point));
+	timespec_add_ms(&time_point, TIMEDWAIT_TIMEOUT_MS);
+
+	zassert_ok(pthread_mutex_lock(mtx));
+	ret = pthread_cond_timedwait(&cond_wait_cv, mtx, &time_point);
+	zassert_ok(pthread_mutex_unlock(mtx));
+
+	return INT_TO_POINTER(ret);
+}
+
+static void test_pthread_cond_timedwait(void)
+{
+	void *ret;
+	pthread_t th;
+
+	posix_test_skip_if_native_libc();
+
+	zassert_ok(pthread_mutex_init(&cond_wait_mtx, NULL));
+	zassert_ok(pthread_cond_init(&cond_wait_cv, NULL));
+
+	printk("Expecting timedwait with timeout of %d ms to fail\n", TIMEDWAIT_TIMEOUT_MS);
+	zassert_ok(pthread_create(&th, NULL, cond_timedwait_fn, &cond_wait_mtx));
+	zassert_ok(pthread_join(th, &ret));
+	zassert_equal(ETIMEDOUT, POINTER_TO_INT(ret));
+
+	printk("Expecting timedwait with timeout of %d ms to succeed after %d ms\n",
+	       TIMEDWAIT_TIMEOUT_MS, TIMEDWAIT_TIMEOUT_DELAY_MS);
+	zassert_ok(pthread_create(&th, NULL, cond_timedwait_fn, &cond_wait_mtx));
+	k_msleep(TIMEDWAIT_TIMEOUT_DELAY_MS);
+	zassert_ok(pthread_mutex_lock(&cond_wait_mtx));
+	zassert_ok(pthread_cond_signal(&cond_wait_cv));
+	zassert_ok(pthread_mutex_unlock(&cond_wait_mtx));
+	zassert_ok(pthread_join(th, &ret));
+	zassert_equal(0, POINTER_TO_INT(ret));
+
+	IF_NOT_NATIVE_LIBC({
+		zassert_ok(pthread_mutex_lock(&cond_wait_mtx));
+		zassert_equal(EINVAL, pthread_cond_timedwait(&cond_wait_cv, &cond_wait_mtx, NULL));
+		zassert_ok(pthread_mutex_unlock(&cond_wait_mtx));
+	})
+
+	zassert_ok(pthread_cond_destroy(&cond_wait_cv));
+	zassert_ok(pthread_mutex_destroy(&cond_wait_mtx));
+}
+
+ZTEST_THREADS_BASE(test_pthread_cond_timedwait);
