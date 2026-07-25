10455e6a5e4ccaa37d1c3bc8c3dcf5aa31325bff
Chris Friedt <chris@fr4.co>
2026-07-24 20:21:03 -0400

    tests: posix: single_process: add basic ISO C tests
    Exercise the ISO C subset of POSIX_SINGLE_PROCESS: errno assignment and
    getenv of an unset name.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

---
+++ tests/posix/single_process/src/errno.c
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The Zephyr Project Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+
+#include <zephyr/ztest.h>
+
+ZTEST(posix_single_process, test_errno)
+{
+	errno = 0;
+	zassert_equal(errno, 0);
+	errno = ERANGE;
+	zassert_equal(errno, ERANGE);
+	errno = 0;
+}
