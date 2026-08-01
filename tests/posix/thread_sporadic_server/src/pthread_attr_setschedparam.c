f7da758f2b320b27e7e74e4eaf609112c724907c
Chris Friedt <chris@fr4.co>
2026-08-01 07:58:45 -0400

    tests: posix: add the thread_sporadic_server testsuite
    Verify the interface of the Thread Sporadic Server option: the
    _POSIX_THREAD_SPORADIC_SERVER and SCHED_SPORADIC symbols, the
    sched_ss_* members of struct sched_param, and that the scheduling
    functions modified by the option (pthread_attr_getschedparam,
    pthread_attr_setschedparam, pthread_attr_setschedpolicy,
    pthread_getschedparam, and pthread_setschedparam) accept the
    SCHED_SPORADIC policy, honor sched_priority, and reject invalid
    sporadic server parameters with EINVAL. Behavioural coverage is
    intentionally limited to symbol availability and callability, since
    execution-time budgets are not enforced by the stub implementation.
    
    There is no linux_compat variant because glibc does not provide
    SCHED_SPORADIC or the sched_ss_* members.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

---
+++ tests/posix/thread_sporadic_server/src/pthread_attr_setschedparam.c
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The Zephyr Project Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <limits.h>
+#include <pthread.h>
+#include <sched.h>
+
+#include <zephyr/ztest.h>
+
+#include "thread_sporadic_server_tests.h"
+
+ZTEST(posix_thread_sporadic_server, test_pthread_attr_setschedparam)
+{
+	pthread_attr_t attr;
+	struct sched_param param = sporadic_param(sched_get_priority_min(SCHED_SPORADIC));
+
+	zassert_ok(pthread_attr_init(&attr));
+	zassert_ok(pthread_attr_setschedpolicy(&attr, SCHED_SPORADIC));
+	zassert_ok(pthread_attr_setschedparam(&attr, &param));
+
+	param.sched_ss_max_repl = 0;
+	zassert_equal(pthread_attr_setschedparam(&attr, &param), EINVAL);
+	param.sched_ss_max_repl = _POSIX_SS_REPL_MAX + 1;
+	zassert_equal(pthread_attr_setschedparam(&attr, &param), EINVAL);
+
+	zassert_ok(pthread_attr_destroy(&attr));
+}
